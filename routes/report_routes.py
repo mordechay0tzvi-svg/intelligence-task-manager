91b844cef669326fd56e6d6c89f777c1d3532211
Mordechai Bar-tal <mordechay0tzvi@gmail.com>
2026-06-18 16:17:35 +0300

    logs for report routes and finnished all BH

--- routes/report_routes.py
+++ routes/report_routes.py
@@ -1,20 +1,22 @@
 from database.mission_db import mdb
 from database.agent_db import adb
-
 from fastapi import Body, Query, APIRouter, HTTPException
+from logs.logger import logger
 
 router = APIRouter()
 
 @router.get("/reports/summary")
 def summary():
+    logger.info("attempt to get summary")
     if not adb.get_all_agents() or not mdb.get_all_missions():
+        logger.info("no data")
         return {"active_agents_count": 0,
                 "total_missions": 0,
                 "open_missions": 0,
                 "completed_missions": 0,
                 "failed_missions": 0,
                 "critical_missions": 0}
-    
+    logger.info("showing summary")
     return {
             "active_agents_count": adb.count_active_agents(),
             "total_missions": len(mdb.get_all_missions()),
@@ -27,8 +29,11 @@ def summary():
 
 @router.get("/reports/missions-by-status")
 def missions_by_status():
+    logger.info("attempt to get missions-by-status")
     if not adb.get_all_agents() or not mdb.get_all_missions():
+        logger.info("no data")
         return {"open": 0,"in_progress": 0,"completed": 0,"failed": 0,"cancelled": 0}
+    logger.info("showing missions-by-status")
     return {
             "open": mdb.count_open_missions(),
             "in_progress": mdb.count_by_status("IN_PROGRESS"),
@@ -40,6 +45,9 @@ def missions_by_status():
 
 @router.get("/reports/top-agent")
 def top_agent():
+    logger.info("attempt to get top agent")
     if not adb.get_all_agents():
+        logger.info("no data")
         raise HTTPException(404, "No agents")
+    logger.info("showing top agent")
     return mdb.get_top_agent()
